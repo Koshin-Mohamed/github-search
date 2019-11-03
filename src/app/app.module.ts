a424e51a5ee55bac2fc0ac47ef51063354fc3847
Koshin-Mohamed <koshin.moh@gmail.com>
2019-11-03 17:18:25 +0300

    add jumbotron and with title and placed the searchbar within it.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchFormComponent } from './search-form/search-form.component';
+import { DisplayUserComponent } from './display-user/display-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchFormComponent
+    SearchFormComponent,
+    DisplayUserComponent
   ],
   imports: [
     BrowserModule,
