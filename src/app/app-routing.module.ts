b29d2cf75e97ad02d11ac506b1b8dc685432e8ec
Koshin-Mohamed <koshin.moh@gmail.com>
2019-12-07 21:38:22 +0300

    recreate landing-page component as well as style tit along with the details component. Also routing was added between the components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { DetailsComponent } from './details/details.component';
 
-
-const routes: Routes = [];
-
+const routes: Routes = [
+{path: "landing-page", component: LandingPageComponent},
+{path: "details", component: DetailsComponent},
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DisplayUserComponent } from './display-user/display-user.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
 import { DisplayRepoComponent } from './display-repo/display-repo.component';
 import { FormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http';
 import { DetailsComponent } from './details/details.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DisplayUserComponent,
-    LandingPageComponent,
     DisplayRepoComponent,
-    DetailsComponent
+    DetailsComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
